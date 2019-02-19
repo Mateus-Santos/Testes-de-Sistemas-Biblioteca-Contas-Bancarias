0e3caf81a8d625c19e50eccf1715564c6f995a4d
layssbp <laysbrito17@gmail.com>
2019-02-19 20:28:16 -0300

    Versão final dos testes

--- cod_teste_banco.py
+++ cod_teste_banco.py
@@ -5,25 +5,25 @@ import sistema_banco
 
 
 class MyModuleTest(unittest.TestCase):
-	def teste_calc1(self):
+	def teste01_calc1(self):
 		self.assertEqual(sistema_banco.Criacao_conta(["1", "12312312325", "Lucas", "1234"]), "Usuário cadastrado com sucesso")
-	def teste_calc12(self):
+	def teste10_calc12(self):
 		self.assertEqual(sistema_banco.Deletar_conta(["12312312325", "1234"]), "Conta deletada com sucesso")
-	def teste_calc13(self):
+	def teste02_calc2(self):
 		self.assertEqual(sistema_banco.Alterar_limite(["12312312325", "1234"]), "Limite alterado com sucesso")
-	def teste_calc14(self):
+	def teste03_calc3(self):
 		self.assertEqual(sistema_banco.Receber_emprestimo(["12312312325", "1234"]), "Operação feita com sucesso")
-	def teste_calc15(self):
+	def teste04_calc4(self):
 		self.assertEqual(sistema_banco.Transferencia_bancaria(["12312312325", "1234"]), "Operação feita com sucesso")
-	def teste_calc16(self):
+	def teste05_calc5(self):
 		self.assertEqual(sistema_banco.Saque(["12312312325", "1234"]), "Operação feita com sucesso")
-	def teste_calc17(self):
+	def teste06_calc6(self):
 		self.assertEqual(sistema_banco.Solicitar_emprestimo(["12312312325", "1234"]), "Operação feita com sucesso")
-	def teste_calc18(self):
+	def teste07_calc7(self):
 		self.assertEqual(sistema_banco.Verificar_saldo(["12312312325", "1234"]), "Operação feita com sucesso")
-	def teste_calc19(self):
+	def teste08_calc8(self):
 		self.assertEqual(sistema_banco.Verificar_limite(["12312312325", "1234"]), "Operação feita com sucesso")
-	def teste_calc110(self):
+	def teste11_calc9(self):
 		self.assertEqual((["12312312325", "1234"]), "Operação feita com sucesso")
 
 
--- sistema_biblioteca.py
+++ sistema_biblioteca.py
@@ -1,4 +1,5 @@
 livros = []
+livros_emprestados = []
 
 def cad_livro(livro):
 	livros.append(livro)
@@ -14,36 +15,36 @@ def excluir_livro(excluir):
 
 
 def verificar(verifique):
-	for v in livros:
-		if (v[1] == verifique):
+	for livro in livros:
+		if (livro[1] == verifique):
 			return "Livro encontrado"
 	return "Esse livro não existe." 
 
 
 def listar_livros():
-	for l in livros:
-		print(l)
-
+	for livro in livros:
+		print(livro)
 	return "Livros listados"
 
 
 def listar_livros_emprestados():
-	for l in livros:
-		print(l)
+	for livro in livros_emprestados:
+		print(livro)
 	return "Livros emprestados foram listados"
 
 
-def emprestimo(livro):
+def emprestimo(livro_id):
 	for l in livros:
-		if(l[1] == livro):
-			livros.remove(livro)
+		if(l[1] == livro_id):
+			livros_emprestados.append(l)
+			livros.remove(l)
 			return "Livro emprestado com sucesso."
 	return "Este livro já foi emprestado."
 
-def devolucao(livro):
-	for l in livros:
-		if(l[1] == livro):
-			return "Livro já foi devolvido"
-	#precisa de mais informações do livro para devolver de verdade...
-	return "Livro devolvido com sucesso."
-
+def devolucao(livro_id):
+	for l in livros_emprestados:
+		if(l[1] == livro_id):
+			livros.append(l)
+			livros_emprestados.remove(l)
+			return "Livro devolvido com sucesso."
+	return "Empréstimo não encontrado."
